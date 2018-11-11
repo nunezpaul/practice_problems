5e86c1b2933b27b0d28ac520e774eee53754f4d5
nunezpaul <nunez.paul@utexas.edu>
2018-11-11 00:54:09 -0800

    implemented linkedhashmap solution

--- max_substring_m_unique_chars/solution.py
+++ max_substring_m_unique_chars/solution.py
@@ -1,64 +1,136 @@
+class DoublyLinkedList(object):
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+    def insert_next(self, val):
+        next = DoublyLinkedList(val)
+        self.next = next
+        next.prev = self
+
+    def insert_prev(self, val):
+        prev = DoublyLinkedList(val)
+        self.prev = prev
+        prev.next = self
+
+
+
+class LinkedHashMap(object):
+    def __init__(self):
+        self.linked_list = DoublyLinkedList('start')
+        self.linked_list.insert_next('end')
+        self.shortcut_maps = {'start': self.linked_list, 'end': self.linked_list.next}
+
+    def push(self, val):
+        end = self.shortcut_maps['end']
+        prev_last = end.prev
+        new_last = DoublyLinkedList(val)
+
+        prev_last.next = new_last
+        new_last.prev = prev_last
+        end.prev = new_last
+        new_last.next = end
+
+        self.shortcut_maps[val] = new_last
+
+    def pop(self, val=None):
+        old_first = self.shortcut_maps[val] if val else self.shortcut_maps['start'].next
+        new_first = old_first.next  # previously second in the list
+        start = old_first.prev
+
+        start.next = new_first
+        new_first.prev = start
+
+        self.shortcut_maps.pop(old_first.val)
+
+        return old_first.val
+
+    def contains(self, val):
+        return val in self.shortcut_maps
+
+    def get_list(self):
+        runner = self.shortcut_maps['start']
+        result = [runner.val]
+        while runner.next:
+            result.append(runner.next.val)
+            runner = runner.next
+        return result
+
+
 class StringQueue(object):
     def __init__(self, unique_char_limit):
-        self.string = []
-        self.last_unique_chars_idx = {}
-        self.num_unique_chars = 0
         self.max_allowable_unique_chars = unique_char_limit
-        self.length = 0
+        self.last_char_idx = {}
+        self.linked_hashmap = LinkedHashMap()
         self.max_length = 0
-        self.string_length = 0
+        self.start_idx = 0
+        self.curr_idx = 1
+        self.num_unique_chars = 0
 
     def add_char(self, char):
-        char_seen_before = char in self.last_unique_chars_idx
+        char_seen_before = self.linked_hashmap.contains(char)
         can_add_another_unique = self.num_unique_chars + 1 <= self.max_allowable_unique_chars
+        self.last_char_idx[char] = self.curr_idx
+
+
         if char_seen_before:
-            self._add_new_char(char, char_seen_before)
+            self.linked_hashmap.pop(char)
+            self.linked_hashmap.push(char)
         elif can_add_another_unique:
-            self._add_new_char(char, char_seen_before)
+            self.linked_hashmap.push(char)
+            self.num_unique_chars += 1
         else:
-            self._add_new_unique_char(char)
+            prev_char = self.linked_hashmap.pop()
+            self.linked_hashmap.push(char)
+            self.start_idx = self.last_char_idx[prev_char]
+            self.last_char_idx.pop(prev_char)
 
-    def _add_new_char(self, char, char_seen_before):
-        self.num_unique_chars += 1 if not char_seen_before else 0
-        self.string.append(char)
-        self.length += 1
-        self.last_unique_chars_idx[char] = self.length - 1
-        self.max_length = max(self.max_length, self.length)
+        self.max_length = max(self.max_length, self.curr_idx - self.start_idx)
+        self.curr_idx += 1
 
-    def _add_new_unique_char(self, char):
-        key_to_remove = self._find_minimum_to_remove()
-        reset_val = self.last_unique_chars_idx[key_to_remove] + 1
-        self._update_internal_values(key_to_remove, reset_val)
-        self.add_char(char)
-
-    def _find_minimum_to_remove(self):
-        min_val = float('inf')
-        min_key = None
-        for key, val in self.last_unique_chars_idx.items():
-            if val < min_val:
-                min_val = val
-                min_key = key
-        return min_key
-
-    def _update_internal_values(self, key_to_remove, reset_val):
-        for key, val in self.last_unique_chars_idx.items():
-            self.last_unique_chars_idx[key] = val - reset_val
-        self.string = self.string[reset_val:]
-        self.last_unique_chars_idx.pop(key_to_remove)
-        self.num_unique_chars -= 1
-        self.length -= reset_val
 
 def find_max_substring(chars, unique_char_limit):
     string_queue = StringQueue(unique_char_limit)
 
     for char in chars:
         string_queue.add_char(char)
+        string_queue.linked_hashmap.get_list()
 
     return string_queue.max_length
 
+def linked_hashmap_test():
+    linked_hashmap = LinkedHashMap()
+    linked_hashmap.push('a')
+    linked_hashmap.push('b')
+    linked_hashmap.push('c')
+    linked_hashmap.push('d')
+    linked_hashmap.push('e')
+
+    assert (['start', 'a', 'b', 'c', 'd', 'e', 'end'] == linked_hashmap.get_list())
+
+    assert 'c' == linked_hashmap.pop('c')
+    assert ['start', 'a', 'b', 'd', 'e', 'end'] == linked_hashmap.get_list()
+
+    assert 'a' == linked_hashmap.pop()
+    assert ['start', 'b', 'd', 'e', 'end'] == linked_hashmap.get_list()
+
+    assert 'b' == linked_hashmap.pop()
+    assert ['start', 'd', 'e', 'end'] == linked_hashmap.get_list()
+
+    assert 'e' == linked_hashmap.pop('e')
+    assert ['start', 'd', 'end'] == linked_hashmap.get_list()
+
+    assert ['start', 'd', 'end'] == linked_hashmap.get_list()
+    print('Test passed!')
+
 if __name__ == '__main__':
-    chars = 'aaaaaababcbaaabbdef'
-    print(find_max_substring(chars, unique_char_limit=3))
+    chars = 'aababcbaaabbdef'
+    for i in range(1, 8):
+        result = find_max_substring(chars, unique_char_limit=i)
+        print('Longest substring for {i} unique chars is {result}'.format(i=i, result=result))
+        print('----------')
 
     chars = ''
-    print(find_max_substring(chars, unique_char_limit=2))
+    result = find_max_substring(chars, unique_char_limit=2)
+    print('Longest substring for {i} unique chars is {result}'.format(i=2, result=result))
