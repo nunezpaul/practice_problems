c4005bc906d337f54ebd3bc12915bb684f451536
nunezpaul <nunez.paul@utexas.edu>
2018-11-23 14:42:48 -0800

    added hard problem and backtracking

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -17,25 +17,32 @@ class Cell(object):
         self.missing_row_nums = board.missing_row_nums[self.y]
         self.missing_col_nums = board.missing_col_nums[self.x]
         self.missing_square_nums = board.missing_square_nums[board._determine_square_num(self.x, self.y)]
+        self.valid_nums = None
 
-    def has_singleton(self):
-        valid_nums = self.missing_square_nums.intersection(self.missing_row_nums, self.missing_col_nums)
-        if len(valid_nums) == 1:
-            self.val = valid_nums.pop()
-            self._update_possibilities(self.val)
-            return True
-        return False
+    def get_valid_nums(self):
+        return self.missing_square_nums.intersection(self.missing_row_nums, self.missing_col_nums)
 
-    def _update_possibilities(self, only_valid_num):
+    def has_singleton_solution(self):
+        self.valid_nums = self.get_valid_nums()
+        return len(self.valid_nums) == 1
+
+    def update_val_with_singleton(self):
+        if len(self.valid_nums) == 1:
+            self.val = self.valid_nums.pop()
+            self._remove_possibility(self.val)
+
+    def _remove_possibility(self, only_valid_num):
         for category in (self.missing_row_nums, self.missing_col_nums, self.missing_square_nums):
-            if only_valid_num in category:
-                category.remove(only_valid_num)
+            category.remove(only_valid_num)
+
+    def _reinsert_possibility(self, valid_num):
+        for category in (self.missing_row_nums, self.missing_col_nums, self.missing_square_nums):
+            category.add(valid_num)
 
 
 class Board(object):
     def __init__(self, board):
         self.board = board
-        self.print_board()
         self.height = len(board)
         self.width = len(board[0])
         self.missing_row_nums = self._get_missing_row_nums()
@@ -98,23 +105,46 @@ class Board(object):
                     missing_cells[(x, y)] = Cell(self, num, x, y)
         return missing_cells
 
-    def _get_cell_idx(self, x, y):
-        return x + self.height * y
-
     def solve(self):
+        print('Unsolved:')
+        self.print_board()
         board_has_changed = True
         while board_has_changed:
             num_missing_cells_before = len(self.missing_cells)
             for _ in range(num_missing_cells_before):
                 missing_cell_id, missing_cell = self.missing_cells.popitem(last=False)
-                if missing_cell.has_singleton():
+                if missing_cell.has_singleton_solution():
+                    missing_cell.update_val_with_singleton()
                     self.update_value(missing_cell.val, missing_cell.x, missing_cell.y)
-                    print(missing_cell_id)
-                    self.print_board()
                 else:
                     self.missing_cells[missing_cell_id] = missing_cell
             if len(self.missing_cells) == num_missing_cells_before:
                 board_has_changed = False
+
+        if len(self.missing_cells) > 0:
+            self.backtrack_solver()
+
+        print('Solved: ')
+        self.print_board()
+
+    def backtrack_solver(self, ):
+        if len(self.missing_cells) == 0:
+            return True
+
+        missing_cell_id, missing_cell = self.missing_cells.popitem(last=False)
+        valid_nums = missing_cell.get_valid_nums()
+
+        for valid_num in valid_nums:
+            missing_cell._remove_possibility(valid_num)
+            is_valid_path = self.backtrack_solver()
+            if is_valid_path:
+                missing_cell.val = valid_num
+                self.update_value(valid_num, missing_cell_id[0], missing_cell_id[1])
+                return is_valid_path
+            missing_cell._reinsert_possibility(valid_num)
+
+        self.missing_cells[missing_cell_id] = missing_cell
+        return False
 
     def print_board(self):
         for row in self.board:
@@ -123,6 +153,7 @@ class Board(object):
         print('\n')
 
 
+# Easy board
 def generate_board1():
     board = [
         [8, 0, 5, 0, 4, 0, 0, 9, 0],
@@ -138,7 +169,7 @@ def generate_board1():
 
     return board
 
-
+# Hard board
 def generate_board2():
     board = [
         [int(i) for i in '006000134'],
@@ -155,13 +186,12 @@ def generate_board2():
 
 
 if __name__ == '__main__':
+    print('Board 1:')
     board = generate_board1()
     sudoku_puzzle = Board(board)
     sudoku_puzzle.solve()
 
+    print('Board 2:')
     board = generate_board2()
     sudoku_puzzle = Board(board)
-    print(len(sudoku_puzzle.missing_cells))
-    sudoku_puzzle.print_board()
     sudoku_puzzle.solve()
-    print(len(sudoku_puzzle.missing_cells))
