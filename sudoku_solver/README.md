344dc6ade94482659f1dbda54151d8e1c352d610
nunezpaul <nunez.paul@utexas.edu>
2018-11-22 22:00:31 -0800

    simple solution. does not work for harder problems

---
+++ sudoku_solver/README.md
@@ -0,0 +1,1 @@
+Create a program that can solve a sudoku puzzle.
---
+++ sudoku_solver/sudoku_solver.py
@@ -0,0 +1,167 @@
+import math
+import collections
+
+
+class Cell(object):
+    def __repr__(self):
+        return 'C{val}'.format(val=self.val)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __init__(self, board, val, x, y):
+        self.val = val
+        self.change = False
+        self.x = x
+        self.y = y
+        self.missing_row_nums = board.missing_row_nums[self.y]
+        self.missing_col_nums = board.missing_col_nums[self.x]
+        self.missing_square_nums = board.missing_square_nums[board._determine_square_num(self.x, self.y)]
+
+    def has_singleton(self):
+        valid_nums = self.missing_square_nums.intersection(self.missing_row_nums, self.missing_col_nums)
+        if len(valid_nums) == 1:
+            self.val = valid_nums.pop()
+            self._update_possibilities(self.val)
+            return True
+        return False
+
+    def _update_possibilities(self, only_valid_num):
+        for category in (self.missing_row_nums, self.missing_col_nums, self.missing_square_nums):
+            if only_valid_num in category:
+                category.remove(only_valid_num)
+
+
+class Board(object):
+    def __init__(self, board):
+        self.board = board
+        self.print_board()
+        self.height = len(board)
+        self.width = len(board[0])
+        self.missing_row_nums = self._get_missing_row_nums()
+        self.missing_col_nums = self._get_missing_col_nums()
+        self.missing_square_nums = self._get_missing_square_nums()
+        self.missing_cells = self._get_missing_cells()
+
+    def update_value(self, val, x, y):
+        board[y][x] = val
+
+    def update_board(self):
+        self.missing_row_nums = self._get_missing_row_nums()
+        self.missing_col_nums = self._get_missing_col_nums()
+        self.missing_square_nums = self._get_missing_square_nums()
+
+    def get_cell_val(self, x, y):
+        return self.board[y][x]
+
+    def _get_missing_row_nums(self):
+        missing_row_nums = []
+        for row in self.board:
+            missing_nums = set(range(1, 10))
+            for num in row:
+                if num in missing_nums:
+                    missing_nums.remove(num)
+            missing_row_nums.append(missing_nums)
+        return missing_row_nums
+
+    def _get_missing_col_nums(self):
+        missing_col_nums = []
+        for x in range(self.width):
+            missing_nums = set(range(1, 10))
+            for y in range(self.height):
+                num = self.get_cell_val(x, y)
+                if num in missing_nums:
+                    missing_nums.remove(num)
+            missing_col_nums.append(missing_nums)
+        return missing_col_nums
+
+    def _get_missing_square_nums(self):
+        missing_square_nums = [set(range(1,10)) for _ in range(self.height)]
+        for y in range(self.height):
+            for x in range(self.width):
+                square_idx = self._determine_square_num(x, y)
+                num = self.get_cell_val(x, y)
+                if num in missing_square_nums[square_idx]:
+                    missing_square_nums[square_idx].remove(num)
+        return missing_square_nums
+
+    def _determine_square_num(self, x, y):
+        row_num = math.floor(y / 3)
+        col_num = math.floor(x / 3)
+        return row_num * 3 + col_num
+
+    def _get_missing_cells(self):
+        missing_cells = collections.OrderedDict()
+        for y, row in enumerate(self.board):
+            for x, num in enumerate(row):
+                if num == 0:
+                    missing_cells[(x, y)] = Cell(self, num, x, y)
+        return missing_cells
+
+    def _get_cell_idx(self, x, y):
+        return x + self.height * y
+
+    def solve(self):
+        board_has_changed = True
+        while board_has_changed:
+            num_missing_cells_before = len(self.missing_cells)
+            for _ in range(num_missing_cells_before):
+                missing_cell_id, missing_cell = self.missing_cells.popitem(last=False)
+                if missing_cell.has_singleton():
+                    self.update_value(missing_cell.val, missing_cell.x, missing_cell.y)
+                    print(missing_cell_id)
+                    self.print_board()
+                else:
+                    self.missing_cells[missing_cell_id] = missing_cell
+            if len(self.missing_cells) == num_missing_cells_before:
+                board_has_changed = False
+
+    def print_board(self):
+        for row in self.board:
+            str_row = [str(num) for num in row]
+            print(' '.join(str_row))
+        print('\n')
+
+
+def generate_board1():
+    board = [
+        [8, 0, 5, 0, 4, 0, 0, 9, 0],
+        [0, 6, 9, 7, 1, 0, 0, 0, 3],
+        [7, 0, 0, 3, 0, 8, 0, 0, 0],
+        [0, 0, 6, 0, 0, 0, 2, 4, 0],
+        [5, 4, 0, 0, 3, 0, 0, 1, 8],
+        [0, 1, 8, 0, 0, 0, 9, 0, 0],
+        [0, 0, 0, 8, 0, 4, 0, 0, 9],
+        [1, 0, 0, 0, 7, 3, 4, 5, 0],
+        [0, 7, 0, 0, 5, 0, 3, 0, 2]
+     ]
+
+    return board
+
+
+def generate_board2():
+    board = [
+        [int(i) for i in '006000134'],
+        [int(i) for i in '300400009'],
+        [int(i) for i in '080100000'],
+        [int(i) for i in '600500010'],
+        [int(i) for i in '000761000'],
+        [int(i) for i in '030008007'],
+        [int(i) for i in '000009080'],
+        [int(i) for i in '100004002'],
+        [int(i) for i in '245000600'],
+    ]
+    return board
+
+
+if __name__ == '__main__':
+    board = generate_board1()
+    sudoku_puzzle = Board(board)
+    sudoku_puzzle.solve()
+
+    board = generate_board2()
+    sudoku_puzzle = Board(board)
+    print(len(sudoku_puzzle.missing_cells))
+    sudoku_puzzle.print_board()
+    sudoku_puzzle.solve()
+    print(len(sudoku_puzzle.missing_cells))
