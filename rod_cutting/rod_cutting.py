d621fda2fb85e91fdca06e25292efdbef4823c53
nunezpaul <nunez.paul@utexas.edu>
2018-12-12 18:56:13 -0800

    added test

--- rod_cutting/rod_cutting.py
+++ rod_cutting/rod_cutting.py
@@ -24,5 +24,6 @@ def cut_rod(curr_len, prices, memo):
 
 if __name__ == '__main__':
     prices = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
-    for r0d_l3ngth in range(11):
-        print(optimal_cuts(r0d_l3ngth, prices))
+    results = [0, 1, 5, 8, 10, 13, 17, 18, 22, 25, 30]
+    for result, r0d_l3ngth in zip(results, range(11)):
+        assert result == optimal_cuts(r0d_l3ngth, prices)
